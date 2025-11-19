31fdfdd75b555988137a247e8edb86cc274ff3df
gauravvgithub <gauravraj7011@gmail.com>
2025-11-19 14:31:40 +0530

    removing unsed files

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { HelmetProvider } from "react-helmet-async";
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
 
 
 const container = document.getElementById('root');
@@ -15,7 +14,3 @@ root.render(
     </HelmetProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
