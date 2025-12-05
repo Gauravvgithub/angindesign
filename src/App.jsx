346fc3c18392109e3b233100c3c663d843db2da9
gauravvgithub <gauravraj7011@gmail.com>
2025-12-05 11:30:12 +0530

    meta in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { Helmet } from "react-helmet-async";
 import Components from "./components/Components";
 import ScrolToTop from "./components/Elements/ScrolToTop";
-// import Loader from "./components/Elements/Loader";
+import Loader from "./components/Elements/Loader";
 import { Toaster } from "react-hot-toast";
 import useGTM from "./useGTM";
 
@@ -13,7 +13,7 @@ const App = () => {
       <div className="App">
         <Components />
         <ScrolToTop />
-        {/* <Loader /> */}
+        <Loader />
       </div>
     </>
   );
